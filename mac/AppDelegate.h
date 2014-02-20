b99d8492de29ea11ebfa84153ba86a9568ceb1db
Alp Yucebiligin <ayucebilgin@Alps-iMac.local>
2014-02-20 15:45:57 -0800

    Initial Commit

---
+++ mac/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mac
+//
+//  Created by Alp Yucebiligin on 2/20/14.
+//  Copyright (c) 2014 Alp Yucebiligin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ mac/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  mac
+//
+//  Created by Alp Yucebiligin on 2/20/14.
+//  Copyright (c) 2014 Alp Yucebiligin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
