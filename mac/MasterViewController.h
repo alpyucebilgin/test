2b14756bc448ff93654817248ca694b1ebf0e34f
Alp Yucebiligin <ayucebilgin@Alps-iMac.local>
2014-02-20 16:11:57 -0800

    first commit

---
+++ mac/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  mac
+//
+//  Created by Alp Yucebiligin on 2/20/14.
+//  Copyright (c) 2014 Alp Yucebiligin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MasterViewController : NSViewController
+
+@end
---
+++ mac/MasterViewController.m
@@ -0,0 +1,26 @@
+//
+//  MasterViewController.m
+//  mac
+//
+//  Created by Alp Yucebiligin on 2/20/14.
+//  Copyright (c) 2014 Alp Yucebiligin. All rights reserved.
+//
+
+#import "MasterViewController.h"
+
+@interface MasterViewController ()
+
+@end
+
+@implementation MasterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+@end
